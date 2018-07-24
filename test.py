f5bcf662d9c81d7ac83c7035f38285d4035cf425
“WilliamTalk” <992621968@qq.com>
2018-07-24 09:18:53 +0800

    remove unnecessary files

--- test.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-Intermountain=pd.read_csv("data/ihc_sample.csv")
-lines = Intermountain[["EMPI","FCILTY_ID", "TX_TYPE_DSC","START_DT","END_DT"]]
-for i in range(len(lines)):
-    print(lines.loc[i,"START_DT"])
